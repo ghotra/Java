c85992bd53e29e6cd2b8906d7a6d1d5c6da72105
Baljinder Ghotra <ghotra.2020@gmail.com>
2018-03-10 21:04:25 -0500

    Check if string are anagrams or not

---
+++ CheckAnagrams/src/com/baljinder/Main.java
@@ -0,0 +1,54 @@
+package com.baljinder;
+
+import java.util.Scanner;
+
+/**
+ * Print "Anagrams" if String1 and String2  are case-insensitive anagrams of each other; otherwise, print "Not Anagrams" instead
+ */
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter the first string");
+        String inputString1 = scanner.nextLine();
+        System.out.println("Enter the second string");
+        String inputString2 = scanner.nextLine();
+
+        inputString1 = sortCharacters(inputString1);
+        inputString2 = sortCharacters(inputString2);
+        if(inputString1.equalsIgnoreCase(inputString2)){
+            System.out.println("String are anagrams");
+        }else{
+            System.out.println("String are not anagrams");
+        }
+    }
+
+    /**
+     *
+     * @param inputString UserInput string to sort characters
+     * @return a string with sorted characters
+     */
+    private static String sortCharacters(String inputString){
+        char temp;
+        //Remove this if the strings are case sensitive
+        inputString = inputString.toLowerCase();
+        char[] charArray = new char[inputString.length()];
+        for(int i=0; i<inputString.length();i++){
+            charArray[i] = inputString.charAt(i);
+        }
+        for(int i =0; i<charArray.length-1;i++){
+            for(int j=i+1;j<charArray.length;j++){
+                if(charArray[j] < charArray[i]){
+                    temp = charArray[i];
+                    charArray[i] = charArray[j];
+                    charArray[j] = temp;
+                }
+            }
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for(char c : charArray){
+            stringBuilder.append(c);
+        }
+        return stringBuilder.toString();
+    }
+}
